a4061d42b9f0239488ad8eac6833bdcabb9fab49
uixneg0 <96099199+uixneg0@users.noreply.github.com>
2022-12-05 22:18:43 -0600

    initial commit

---
+++ src/test/java/com/uixneg0/fedextrackingserver/FedExTrackingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uixneg0.fedextrackingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FedExTrackingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
